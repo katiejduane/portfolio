087472c8197b2411b464f381942cbcfa6bf6abc4
Katie Duane <katiejduane@gmail.com>
2019-03-16 14:54:10 -0400

    portfolio updates after final project

--- projectData.js
+++ projectData.js
@@ -1,13 +1,25 @@
 const projects = [
 
     {
+        title: 'Sift',
+        img: `images/sift/sift.png`,
+        tech: `<span>Made with </span><i class="devicon-react-original" id="react-icon"></i><i class="devicon-nodejs-plain" id="node-icon"></i> <i class="devicon-express-original" id="express-icon"></i>
+        <i class="devicon-postgresql-plain" id="postgres-icon"></i><i class="devicon-html5-plain" id="html-icon"></i> <i class="devicon-css3-plain" id="css-icon"></i> 
+        <i class="devicon-javascript-plain" id="js-icon">`,
+        github: `<a href="https://github.com/katiejduane/Sift" target="blank">GitHub</a>`,
+        liveSite: `<a href="https://sift.katiejduane.com/" target="blank">Live Site</a>`,
+        description: `<p class="description">Sift was made with curious explorers in mind. It allows users to organize and track all the things they want to experience; whether it's a hike, a new coffee shop, or a pottery class, they can add it, favorite it, and review it with Sift.</p>`,
+        contributions: `<p>Contributions: Logo Design, Database Schema and Set-up, Responsive Design, Creation of re-usable React Components for each category, Initialized database connection, CSS debugging, Front End and Back End code for "To-Do" list and "Active" sections, Edit Logic, UI/UX overhaul, Debugging, Mobile Responsiveness, Middleware creation for User Id, Higher order component implementation.</p>`,
+    },
+
+    {
         title: 'emOceans',
         img: `images/emOceans/newmooddesktop2.png`,
         tech: `<span>Made with </span><i class="devicon-nodejs-plain" id="node-icon"></i> <i class="devicon-express-original" id="express-icon"></i>
         <i class="devicon-mysql-plain" id="mysql-icon"></i> <i class="devicon-html5-plain" id="html-icon"></i> <i class="devicon-css3-plain" id="css-icon"></i> 
         <i class="devicon-javascript-plain" id="js-icon">`,
         github: `<a href="https://github.com/katiejduane/emOceans--backEndProject" target="blank">GitHub</a>`,
-        liveSite: `<span>Live site OTW!</span>`,
+        liveSite: `<a href="https://emoceans.katiejduane.com/" target="blank">Live Site</a>`,
         description: `<p class="description">emOceans is a web application that allows you to take note of your mood every day using color and language. By providing an easy-to-use color palette and a word-finding API, we hoped to make the observation and appreciation of shifts in mood more accessible, helpful, and more beautiful.</p>`,
         contributions: `<p>Contributions: Concept, implementation of chroma.js for color manipulation, implementation of datamuse API, router management, registration connection, database schema, database connection, data visualization.</p>`,
     },
